1f90d292e2af10c2d6bb3aa4a09e9e25baaf5eeb
karme <karme>
2004-04-19 18:34:55 +0000

    moved to etc

--- egachine/src/client/config.js
+++
@@ -1,9 +0,0 @@
-// this is the client configuration file - this is javascript
-
-// if you wish to set the screen resolution
-// if you don't set it egachine will try to use the current resolution
-// width=1024;
-//width=320;
-// height=768;
-//height=240;
-// fullscreen=false;
--- egachine/src/server/config.js
+++
@@ -1,4 +0,0 @@
-// this is the server configuration file - this is javascript
-
-// listen on this port
-listenPort=47000;

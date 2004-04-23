0833308971f221d2cefee626392393f44bcae323
karme <karme>
2004-04-23 14:07:39 +0000

    build shared library version of spidermonkey on linux again ship debian package with shared library in /usr/lib/egachine reduced debian package size to ~650kb

--- egachine/common.sh
+++ egachine/common.sh
@@ -4,6 +4,12 @@
 # ./install
 # ./clean
 
+# debug build?
+#DEBUG=1
+
+# verbose compilation
+VERBOSE=1
+
 function run() {
     if [ "x$VERBOSE" != "x" ]; then echo "$1"; fi
     $1
@@ -35,15 +41,19 @@ CXXFLAGS="$CXXFLAGS $DEFINES -D_REENTRANT"
 if [ "x$EGACHINE_CROSS" = "x" ]; then
     PREFIX=${PREFIX:=/usr/local}
     BINDIR=${BINDIR:=$PREFIX/bin}
+    LIBDIR=${LIBDIR:=$PREFIX/lib}
     DATADIR=${DATADIR:=$PREFIX/share}
     DOCDIR=${DOCDIR:=$PREFIX/share/doc}
     SYSCONFDIR=${SYSCONFDIR:=$PREFIX/etc}
+
+    # todo non-linux platforms will use other dirs
+    if [ "x$DEBUG" = "x" ]; then
+	SM_BUILDDIR="Linux_All_OPT.OBJ"
+	BUILD_OPT="BUILD_OPT=1"
+    else
+	SM_BUILDDIR="Linux_All_DBG.OBJ"
+    fi
 fi
 
-# debug build?
-#DEBUG=1
-
-# verbose compilation
-VERBOSE=1
 
 PROGRAMS="egachine egaserver egares"

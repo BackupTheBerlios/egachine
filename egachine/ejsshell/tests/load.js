f68fcfa15f31b10166361e9ea8180af0c0de1b7e
karme <karme>
2004-09-24 09:51:30 +0000

    major rearangement for 0.1.0

--- egachine/ejsshell/tests/load.js
+++
@@ -1,2 +0,0 @@
-if (!this.ejs.config) throw new Error("failed");
-
--- egachine/src/etc/server.js
+++
@@ -1,6 +0,0 @@
-/*
-  this is the egaserver configuration file - this is javascript
-*/
-
-// port to listen on for incoming connections
-listenPort=47000;

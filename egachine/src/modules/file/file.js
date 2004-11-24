54708a6a2feafd9d21f33572e81dcd7b20442f94
karme <karme>
2004-11-24 19:50:59 +0000

    make --disable-module options actually work, don't link ejs against libz

--- egachine/src/modules/file/file.js
+++ egachine/src/modules/file/file.js
@@ -5,6 +5,8 @@
   var fname=ejs.ModuleLoader.findFile(ejs.config.modules.libraryPath,"ejsfile.la");
   if (!fname) throw new Error("Could not find module: 'ejsfile.la'");
   ejs.ModuleLoader.loadNative.call(ejs.getGlobal(),"ejsfile",fname.substring(0,fname.lastIndexOf(".")));
+
+  //! File open modes
   File.mode={
     read:1,
     write:2,
@@ -13,9 +15,20 @@
     binary:16,
     ate:32
   };
+
+  //! create Stream for reading from a file
+  /*!
+    \param name filename of file to open for reading
+    \return Stream object
+  */
   File.read=function(name){
     return File.open(name,File.mode.read);
   }
+  //! create Stream for writing to a file
+  /*!
+    \param name filename of file to open for reading
+    \return Stream object
+  */
   File.write=function(name){
     return File.open(name,File.mode.write|File.mode.trunc);
   }

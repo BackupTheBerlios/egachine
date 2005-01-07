6377b81aacffade3019b974fd8b4f70ce28595d2
karme <karme>
2005-01-07 00:03:45 +0000

    clean up

--- egachine/src/examples/svg/svgviewer.js
+++ egachine/src/examples/svg/svgviewer.js
@@ -7,94 +7,76 @@
   var File=ejs.ModuleLoader.get("File");
   var stderr=ejs.ModuleLoader.get("Stream").stderr;
   var Video=ejs.ModuleLoader.get("Video");
+  var timeOut;
 
   if (!gl.GetIntegerv(GL_STENCIL_BITS)[0])
     stderr.write("Warning no stencil buffer\n");
 
-  reschedule=function(sec){
-    if (sec<0) throw sec;
-    var start=Timer.getTimeStamp();
-    var dt;
+  function getEvents(dt) {
+    var tmp;
+    if (timeOut) {
+      // process timeout
+      if ((timeOut.remain-=dt)<0) {
+	tmp=timeOut;
+	timeout=false;
+	tmp.func();
+      }
+    }
     Input.poll();
-    if (!sec) sec=0.01;
-    while ((dt=((Timer.getTimeStamp()-start)/1000000))<sec)
-      Timer.uSleep((sec-dt)*1000000);
-    return dt;
+    return true;
+  };
+
+  function step(f,time) {
+    var stepSize=1000000/85*2;
+    if (time && (stepSize>time)) stepSize=time;
+
+    var start,last,now;
+    start=last=now=Timer.getTimeStamp();
+    while ((!time)||(now-start<time)) {
+      Timer.uSleep(stepSize);
+      now=Timer.getTimeStamp();
+      if (!f(now-last)) break;
+      last=now;
+    };
+    return now-start;
   }
 
+  //! called from native code (animation running)
+  reschedule=function(sec){
+    if (sec<0) throw sec;
+    if (!sec) sec=0.001;
+    return step(getEvents,sec*1000000)/1000000;
+  }
+
+  //! called from native code (animation running)
   redisplay=function()
   {
     gl.Clear(GL_COLOR_BUFFER_BIT|GL_STENCIL_BUFFER_BIT);
-    svgl.display(document);
+    svgl.display();
     Video.swapBuffers();
   }
-
-  SVGViewer={
-    debug:function(x){stderr.write(x+"\n");},
-    run:function(){
-      var frame=0,start,last,now,dt,i,timeOut;
-      last=now=start=Timer.getTimeStamp();
-      while(true) {
-	now=Timer.getTimeStamp();
-	dt=now-last;
-	last=now;
-	gl.Clear(GL_COLOR_BUFFER_BIT|GL_STENCIL_BUFFER_BIT);
-
-	if (this.timeOut) {
-	  // process timeout
-	  timeOut=this.timeOut;
-	  if ((timeOut.remain-=dt)<0) {
-	    this.timeOut=false;
-	    timeOut.func();
-	  }
-	}
-
-	Input.poll();
-	svgl.display(document);
-	Video.swapBuffers();
-	++frame;
-      };
-    }
-  };
 
   if (argv.length<2) {
     stderr.write("Usage: "+argv[0]+" FILE\n");
     ejs.exit(false);
   };
 
-  stream=File.read(argv[1]);
+  var stream=File.read(argv[1]);
   if (stream.inAvailable()<=0)
     throw Error("Could not open file: "+argv[1]);
+
   document=new svgl.SVGDocument(stream.read(stream.inAvailable()));
   document.documentElement=document.getDocumentElement();
-
-  /*
-    hmm
-    dom level 2 does not have keyboard events?
-    dom level 3 events is only a note?
-  
-    Input.handleChar=function(c){
-    // todo: this is not yet complete ;-)
-    if (SVGViewer.onKeyDown) SVGViewer.onKeyDown({
-    preventDefault:function(){},
-    keyCode:c.charCodeAt(0)
-    });
-    };
-    document.documentElement.addEventListener=function(event, handler, bx){
-    // bx = ?
-    if (event == "keydown") {
-    SVGViewer.onKeyDown=handler;
-    }else{
-    stderr.write("Warning: event: "+event+" not supported");
-    }
-    };
-  */
+  svgl.selectDocument(document);
 
   setTimeout=function(toeval,ms) {
     // todo: should it be possible to set multiple timeouts?
-    SVGViewer.timeOut={func:function(){eval(toeval);},timeOut:ms*1000,remain:ms*1000};
+    timeOut={func:function(){eval(toeval);},timeOut:ms*1000,remain:ms*1000};
   };
+
   document._handleScripts(this);
   svgl.startAnimation(document);
-  SVGViewer.run();
+  // animation finished
+  
+  step(function(dt){redisplay();return getEvents(dt);});
  })();

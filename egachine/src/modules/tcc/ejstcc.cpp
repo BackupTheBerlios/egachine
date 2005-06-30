7f6d35f42c8c5e643cb624e3b6cebee7b39fd6cc
karme <karme>
2005-06-30 18:25:26 +0000

    improved expermintal tcc module

--- egachine/src/modules/tcc/ejstcc.cpp
+++ egachine/src/modules/tcc/ejstcc.cpp
@@ -34,6 +34,9 @@ extern "C" {
   const char* lastError=NULL;
 
   static
+  TCCState *tccState=NULL;
+
+  static
   void ejstcc_onerror(void*, const char* msg)
   {
     lastError=msg;
@@ -86,6 +89,107 @@ extern "C" {
 
     return JS_TRUE;
   }
+
+  //! compile and run c-code
+  /*!
+    \todo: fix possible memory leaks
+  */
+  static
+  JSBool
+  ejstcc_compile
+  (JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval)
+  {
+    EJS_CHECK_TRUSTED(cx,obj);
+    EJS_CHECK_NUM_ARGS(cx,obj,1,argc);
+
+    JSString *strtype=JS_ValueToString(cx, argv[0]);
+    if (!strtype) return JS_FALSE;
+    // todo: we loose unicode information here
+    char* ctype=JS_GetStringBytes(strtype);
+    if (!ctype) return JS_FALSE;
+    
+    lastError=NULL;
+
+    if (tcc_compile_string(tccState, ctype)) {
+      std::string m("Compilation failed:");
+      if (lastError)
+	m+=lastError;
+      else
+	m+="reason unknown";
+      EJS_THROW_ERROR(cx, obj, m.c_str());
+    }
+    
+    return JS_TRUE;
+  }
+
+  //! call c function of type void(*)(void)
+  static
+  JSBool
+  ejstcc_relocate
+  (JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval)
+  {
+    EJS_CHECK_TRUSTED(cx,obj);
+    EJS_CHECK_NUM_ARGS(cx,obj,0,argc);
+
+    if (tcc_relocate(tccState))
+      EJS_THROW_ERROR(cx, obj, "Relocation failed");
+
+    return JS_TRUE;
+  }
+
+  //! call c function of type void(*)(void)
+  static
+  JSBool
+  ejstcc_call_vv
+  (JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval)
+  {
+    EJS_CHECK_TRUSTED(cx,obj);
+    EJS_CHECK_NUM_ARGS(cx,obj,1,argc);
+
+    JSString *strtype=JS_ValueToString(cx, argv[0]);
+    if (!strtype) return JS_FALSE;
+    // todo: we loose unicode information here
+    char* ctype=JS_GetStringBytes(strtype);
+    if (!ctype) return JS_FALSE;
+    
+    lastError=NULL;
+
+    typedef void (*void_func_void) (void);
+    long unsigned int addr;
+    if (tcc_get_symbol(tccState, &addr, ctype))
+      EJS_THROW_ERROR(cx, obj, "Could net get symbol");
+    void_func_void func;
+    func=(void_func_void)addr;
+    func();
+
+    return JS_TRUE;
+  }
+
+  //! call c wrapper function
+  static
+  JSBool
+  ejstcc_call
+  (JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval)
+  {
+    EJS_CHECK_TRUSTED(cx,obj);
+    EJS_CHECK_NUM_ARGS(cx,obj,1,argc);
+
+    JSString *strtype=JS_ValueToString(cx, argv[0]);
+    if (!strtype) return JS_FALSE;
+    // todo: we loose unicode information here
+    char* ctype=JS_GetStringBytes(strtype);
+    if (!ctype) return JS_FALSE;
+    
+    lastError=NULL;
+
+    typedef JSBool (*wrapper_func) (JSContext*, JSObject*, uintN, jsval*, jsval*);
+    long unsigned int addr;
+    if (tcc_get_symbol(tccState, &addr, ctype))
+      EJS_THROW_ERROR(cx, obj, "Could net get symbol");
+    wrapper_func func;
+    func=(wrapper_func)addr;
+    return func(cx,obj,argc-1,argv+1,rval);
+  }
   
   //! function called after module is loaded
   /*!
@@ -94,10 +198,29 @@ extern "C" {
   JSBool
   ejstcc_LTX_onLoad(JSContext *cx, JSObject *module)
   {
-    if (!JS_DefineFunction(cx,module,"run",ejstcc_run,0,0)) return JS_FALSE;
+    tccState = tcc_new();
+    if (!tccState) EJS_THROW_ERROR(cx, module, "Could not create tcc state");
+    tcc_set_error_func(tccState, NULL, ejstcc_onerror);
+    tcc_set_output_type(tccState, TCC_OUTPUT_MEMORY);
+    
+    if (!JS_DefineFunction(cx,module,"run", ejstcc_run,0,0)) return JS_FALSE;
+    if (!JS_DefineFunction(cx,module,"compile",ejstcc_compile,0,0)) return JS_FALSE;
+    if (!JS_DefineFunction(cx,module,"relocate",ejstcc_relocate,0,0)) return JS_FALSE;
+    if (!JS_DefineFunction(cx,module,"call",ejstcc_call,0,0)) return JS_FALSE;
+    if (!JS_DefineFunction(cx,module,"callVV",ejstcc_call_vv,0,0)) return JS_FALSE;
+    
     return JS_TRUE;
   }
-
+  
+  JSBool
+  ejstcc_LTX_onUnLoad(JSContext *cx, JSObject *module)
+  {
+    if (tccState) {
+      tcc_delete(tccState);
+      tccState = NULL;
+    }
+    return JS_TRUE;
+  }
 #ifdef __cplusplus
 }
 #endif
--- egachine/src/modules/tcc/tcc.js
+++ egachine/src/modules/tcc/tcc.js
@@ -5,12 +5,22 @@
   ejs.ModuleLoader.loadNative.call(tcc,"ejstcc",fname.substring(0,fname.lastIndexOf(".")));
 
   tcc._run = tcc.run;
-  tcc.run = function(x) {
-    tcc._run("\
-#include <tcclib.h>\n				\
-void ejstcc_compiled_function(){\n"
-	     +x
-	     +"\n\
+  tcc.run = function(x, includes) {
+    tcc._run( (includes ? includes : "#include <tcclib.h>\n")
+	      + "void ejstcc_compiled_function(){\n"
+	      + x
+	      + "\n\
 }");
   }
+  tcc.wrap = function(fname, fbody, includes) {
+    // TODO: platform dependant and requires spidermonkey headers to be installed
+    tcc.compile((includes ? includes : "#define XP_UNIX\n#include <smjs/jsapi.h>\n")
+		+ "JSBool wrap_"+fname+"(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval) {\n"
+		+ fbody
+		+ "\nreturn JS_TRUE;\n"
+		+ "}\n"
+		);
+  }
+
  })(this);
+

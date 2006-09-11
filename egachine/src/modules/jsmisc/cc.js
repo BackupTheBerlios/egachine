dd7dac65ebaae42dc4b96b90d97c2a003c02d672
karme <karme>
2006-09-11 09:54:58 +0000

    added compile2

--- egachine/src/modules/jsmisc/cc.js
+++ egachine/src/modules/jsmisc/cc.js
@@ -1,4 +1,6 @@
 (function(cc){
+  // bind modules we depend upon
+  // todo: we should have a standard (declarative) way to define module dependencies
   var util=ejs.ModuleLoader.get("util");
   var Stream=ejs.ModuleLoader.get("Stream");
   var File=ejs.ModuleLoader.get("File");
@@ -15,29 +17,33 @@
     util.GC();
     return ret;
   }
-  // compile c function
-  cc.compile = function(body, includes) {
-    var stderr=Stream.stderr;
 
+  // create a uniq function name
+  cc.mkFuncName = function () {
+    ++_id;
+    return "ejscc_"+_id;
+  }
+
+  // compile, link and dynamically load c-code and return function named cfunc as JS function
+  cc.compile2 = function(cfunc, code) {
+    var stderr=Stream.stderr;
     var tmpdir=tmp.mkdir("cc");
     var cfile=File.write(tmpdir+"/c.c");
-    _id++;
-    var funcname="ejscc_"+_id;
     // todo: do not use internal spidermonkey knowledge here (setting rval)
-    cfile.write((includes ? includes : "") + "\n"
-		+"int "+funcname+"(void* ejs_cx, void* ejs_obj, unsigned ejs_argv, void* ejs_argc, int* ejs_rval) {\n"
-		+ body
-		+ "\n*ejs_rval=(1<<3)|0x6;return 1;\n"
-		+ "}\n\n");
+    cfile.write(code);
     cfile.close();
     var libname=tmpdir+"/c"+_id+".so";
-    var cmd="gcc -Wall -Werror -fpic -o "+libname+" -shared "+tmpdir+"/c.c >"+tmpdir+"/stdout 2>"+tmpdir+"/stderr";
-    //    stderr.write(cmd+"\n");
+    // todo: gcc should be $CC and options passed should be the options detected by libtool
+    var cppflags=util.buildinfo().CPPFLAGS;
+    cppflags = cppflags ? cppflags+" " : "";
+    cppflags+="-Wall -Werror -fpic";
+    var cmd="gcc "+cppflags+" -o "+libname+" -shared "+tmpdir+"/c.c >"+tmpdir+"/stdout 2>"+tmpdir+"/stderr";
     var module;
     var error;
     try{
+      stderr.write("Running: '"+cmd+"'\n");
       if (posix.system(cmd))
-	throw Error("Compilation failed:\n"+File.read(tmpdir+"/stderr").readAll());
+	throw Error("Compilation failed:\n"+File.read(tmpdir+"/c.c").readAll()+"\n"+File.read(tmpdir+"/stderr").readAll());
       module=new ltdl.Ltmodule(libname);
     }catch(e){
       error=e;
@@ -49,9 +55,19 @@
       File.unlink(libname);
       File.rmdir(tmpdir);
     }
-    if (error)
-      throw error;
-    return module.getWrapper(funcname);
+    if (error) throw error;
+    return module.getWrapper(cfunc);
+  }
+
+  // compile c function body
+  cc.compile = function(body, includes) {
+    var funcname=mkFuncName();
+    // todo: do not use internal spidermonkey knowledge here (setting rval)
+    return cc.compile2(funcname, (includes ? includes : "") + "\n"
+		       +"int "+funcname+"(void* ejs_cx, void* ejs_obj, unsigned ejs_argv, void* ejs_argc, int* ejs_rval) {\n"
+		       + body
+		       + "\n*ejs_rval=(1<<3)|0x6;return 1;\n"
+		       + "}\n\n");
   }
 
  })(this);
